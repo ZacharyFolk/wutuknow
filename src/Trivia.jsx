70559bcd4e153b1ea13794d6c24dbd72c728364f
Zac <zacharyfolk@gmail.com>
2024-04-25 22:43:37 -0700

    ✨ A lot of work.. too much to list but this mostly works now

--- src/Trivia.jsx
+++ src/Trivia.jsx
@@ -1,28 +1,60 @@
-import { useState, useEffect, useContext, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import levenshtein from 'fast-levenshtein';
 import './styles.css';
-export default function Trivia() {
+
+/**--------------------------------------------
+ **               Modal Responses
+ *---------------------------------------------**/
+
+const Correct = ({ value }) => {
+  return <p>Yes! That is correct for ${value}!</p>;
+};
+
+const Incorrect = ({ value, clue }) => {
+  return (
+    <p>
+      Sorry, that is incorrect. You lose -${value}! <br />
+      The correct answer was <b>{clue.answer}</b>
+    </p>
+  );
+};
+
+/**--------------------------------------------
+ **               Utilities
+ *---------------------------------------------**/
+
+function parseString(str) {
+  let unwantedWords = ['the', 'a', 'an', 'i'];
+  let words = str.toLowerCase().split(/[^\w']+/);
+  let filteredWords = words.filter((word) => !unwantedWords.includes(word));
+  return filteredWords.join(' ');
+}
+
+/**--------------------------------------------
+ **               Main Component
+ *---------------------------------------------**/
+
+const Trivia = () => {
   const [game, setGame] = useState([]);
-  const [score, setScore] = useState(0);
-  const [q, setQ] = useState('');
-  const [a, setA] = useState('');
-  const [category, setCategory] = useState('');
-  const [input, setInput] = useState(null);
-  const [isActive, setActive] = useState(false);
-  const [correct, setCorrect] = useState(false);
-  const [incorrect, setIncorrect] = useState(false);
-  const [skipped, setSkipped] = useState(false);
-  const [modalOutput, setModalOutput] = useState('');
-  const [currentValue, setCurrentValue] = useState(0);
-  // const levenshtein = require('fast-levenshtein');
+  const [state, setState] = useState({
+    score: 0,
+    currentClue: {},
+    category: '',
+    input: null,
+    modalOutput: '',
+    activeCategory: null,
+  });
   const answerRef = useRef();
 
-  const processResults = (result) => {
-    setGame(result);
+  const toggleCategory = (index) => {
+    setState((prevState) => ({
+      ...prevState,
+      activeCategory: prevState.activeCategory === index ? null : index,
+    }));
   };
 
   const fetchBoard = async () => {
-    setActive(false);
     let localGame = JSON.parse(sessionStorage.getItem('game-one'));
     if (!localGame) {
       const res = await axios.get(
@@ -30,7 +62,7 @@ export default function Trivia() {
       );
       const result = await res.data;
       console.log(result);
-      processResults(result);
+      setGame(result);
     } else {
       setGame(localGame);
     }
@@ -40,41 +72,47 @@ export default function Trivia() {
     fetchBoard();
   }, []);
 
-  const handleClick = (clue) => {
-    setActive(true);
-    setQ(clue.question);
-    setA(clue.answer);
-    setCategory(clue.category);
-    setCurrentValue(clue.value);
-    answerRef.current.innerHTML = '';
-    answerRef.current.focus();
-  };
+  useEffect(() => {
+    if (state.input != null) {
+      checkAnswer();
+    }
+  }, [state.currentClue, state.input]);
 
-  function parseString(str) {
-    // clean out things so have a more likely match, a lot of the results are surrounded by 'i'
-    let unwantedWords = ['the', 'a', 'an', 'i'];
-    let words = str.toLowerCase().split(/[^\w']+/);
-    let filteredWords = words.filter((word) => !unwantedWords.includes(word));
-    return filteredWords.join(' ');
-  }
+  const handleClick = (e, clue, cat) => {
+    console.log('WHAT CAT ', cat);
+    setState((prevState) => ({
+      ...prevState,
+      currentClue: clue,
+      category: cat,
+      modalOutput: <Answer currentClue={clue} currentCategory={cat} />,
+      inputValue: '',
+      currentValue: parseInt(clue.value.replace('$', ''), 10),
+    }));
+    if (answerRef.current) {
+      answerRef.current.innerHTML = '';
+      answerRef.current.focus();
+    }
+    setTimeout(() => {
+      e.target.className += ' deactivated';
+    }, 500);
+  };
 
   const handleButton = () => {
     let typed = parseString(answerRef.current.textContent);
     if (typed === '') return;
     answerRef.current.innerHTML = '';
-    setInput(typed);
+    setState((prevState) => ({ ...prevState, input: typed }));
   };
+
   const handleKeys = (e) => {
     let code = e.keyCode;
     switch (code) {
       case 13:
         e.preventDefault();
         let typed = parseString(e.target.textContent);
-        // prevent empty input
         if (typed === '') return;
         e.target.innerHTML = '';
-        setInput(typed);
-
+        setState((prevState) => ({ ...prevState, input: typed }));
         break;
       default:
       // console.log('something else');
@@ -82,150 +120,145 @@ export default function Trivia() {
   };
 
   const checkAnswer = () => {
-    const distance = levenshtein.get(input, a);
+    const { input, currentClue } = state;
+    const distance = levenshtein.get(input, currentClue.answer);
     let percentageMatch =
-      (1 - distance / Math.min(input.length, a.length)) * 100;
+      (1 - distance / Math.min(input.length, currentClue.answer.length)) * 100;
     percentageMatch = Math.round(percentageMatch);
-    // Here we set how fuzzy the answer can be...
     if (percentageMatch > 70) {
-      let newScore = parseInt(score) + parseInt(currentValue);
-      setCorrect(true);
-      setModalOutput(Correct);
-      setScore(newScore);
-      setActive(false);
+      let newScore = parseInt(state.score) + parseInt(state.currentValue);
+      setState((prevState) => ({
+        ...prevState,
+        modalOutput: <Correct value={state.currentValue} />,
+        score: newScore,
+      }));
       setTimeout(() => {
-        setCorrect(false);
-        setModalOutput('');
-      }, 2000);
-      // fade result message after a few seconds
+        setState((prevState) => ({ ...prevState, modalOutput: '' }));
+      }, 3000);
     } else {
-      let newScore = parseInt(score) - parseInt(currentValue);
-      setScore(newScore);
-      setActive(false);
-      setIncorrect(true);
-      setModalOutput(Incorrect);
+      let newScore = parseInt(state.score) - parseInt(state.currentValue);
+      setState((prevState) => ({
+        ...prevState,
+        score: newScore,
+        modalOutput: (
+          <Incorrect value={state.currentValue} clue={state.currentClue} />
+        ),
+      }));
       setTimeout(() => {
-        setIncorrect(false);
+        setState((prevState) => ({ ...prevState, modalOutput: '' }));
       }, 3000);
     }
-    setActive(false);
-  };
-  const skipQ = () => {
-    setActive(false);
-    setSkipped(true);
-    setModalOutput(Skip);
-    setTimeout(() => {
-      setSkipped(false);
-      setModalOutput(Skip);
-    }, 3000);
+    setState((prevState) => ({
+      ...prevState,
+      activeCategory: null,
+      input: null,
+    }));
   };
 
-  // Modal Messages
-  const Correct = () => {
-    return <p>Yes! That is correct for ${currentValue}!</p>;
-  };
+  const Answer = ({ currentClue, currentCategory }) => {
+    const [skipped, setSkipped] = useState(false);
+    const skipQ = () => {
+      setSkipped(!skipped);
+      setTimeout(() => {
+        setState((prevState) => ({ ...prevState, modalOutput: '' }));
+      }, 2000);
+    };
 
-  const Incorrect = () => {
-    return (
-      <p>
-        Sorry, that is incorrect. You lose -${currentValue}! <br />
-        The correct answer was <b>{a.toUpperCase()}</b>
-      </p>
-    );
-  };
-
-  const Skip = () => {
     return (
       <>
-        <p>
-          OK, skipping that one. <br />
-          The correct answer was <b>{a.toUpperCase()}</b>
-        </p>
+        {skipped ? (
+          <p>
+            OK, skipping that one. <br />
+            The correct answer was <b>{currentClue.answer}</b>
+          </p>
+        ) : (
+          <div className="qaContent" onClick={() => answerRef.current.focus()}>
+            <div className="heading">
+              <h1>
+                {currentCategory} for ${state.currentValue}
+              </h1>
+            </div>
+            <div
+              className="question"
+              dangerouslySetInnerHTML={{ __html: currentClue.question }}
+            ></div>
+            <div className="answer">
+              <span>
+                What (<i>who</i>) is
+              </span>
+              <span className="qForm">
+                <span
+                  className="terminal-input"
+                  contentEditable="true"
+                  suppressContentEditableWarning={true}
+                  onKeyDown={(e) => handleKeys(e)}
+                  ref={answerRef}
+                ></span>
+                <button className="submitAnswer" onClick={handleButton}>
+                  Submit
+                </button>
+              </span>
+              <div className="skipButton">
+                <button onClick={skipQ}>skip</button>
+              </div>
+            </div>
+          </div>
+        )}
       </>
     );
   };
 
   const newGame = () => {
-    sessionStorage.setItem('game-one', null);
     fetchBoard();
   };
-  useEffect(() => {
-    if (input == null) return;
-    checkAnswer(input);
-  }, [input]);
 
   return (
     <div className="game-container">
       <div className="gameHeader">
         <span className="gameTitle">Wutuno?</span>
-        <span className="score"> SCORE : ${score}</span>
+        <span className="score"> SCORE : ${state.score}</span>
         <button onClick={newGame}>New Game</button>
       </div>
 
       <div className="trivia-board">
         {game.length > 0 &&
           game.map((category, i) => (
-            <div key={category.category} className={'category cat-' + (i + 1)}>
-              <h1>{category.category}</h1>
-
-              {category.clues.map((clue) => (
-                <div
-                  key={clue.value}
-                  className="value"
-                  data-value={clue.value}
-                  data-category={category.category}
-                  data-question={clue.question}
-                  data-answer={clue.answer}
-                  onClick={() => handleClick(clue)}
-                >
-                  ${clue.value}
-                </div>
-              ))}
+            <div key={category.category}>
+              <h1
+                className={`category cat-${i + 1} ${
+                  state.activeCategory === i ? 'active' : ''
+                }`}
+                onClick={() => toggleCategory(i)}
+              >
+                {category.category}
+              </h1>
+              <div
+                className={`clue ${state.activeCategory === i ? 'on' : 'off'}`}
+              >
+                {category.clues.map((clue) => (
+                  <div
+                    key={clue.value}
+                    className="value"
+                    data-value={clue.value}
+                    data-category={category.category}
+                    data-question={clue.question}
+                    data-answer={clue.answer}
+                    onClick={(e) => handleClick(e, clue, category.category)}
+                  >
+                    {clue.value}
+                  </div>
+                ))}
+              </div>
             </div>
           ))}
       </div>
-      {/* TODO : This could probably be cleaned up since everything uses same generic modal */}
-      <div
-        className={
-          (correct || incorrect || skipped ? 'active ' : 'inactive ') +
-          ' game-modal'
-        }
-      >
-        {modalOutput}
-      </div>
-
-      <div
-        className={'qaBox ' + (isActive ? 'active' : 'inactive')}
-        onClick={() => answerRef.current.focus()}
-      >
-        <div className="qaContent">
-          <div className="heading">
-            <h1>
-              {category} for ${currentValue}
-            </h1>
-          </div>
-          <div className="question">{q}</div>
-          <div className="answer">
-            <span>
-              What (<i>who</i>) is
-            </span>
-            <span
-              className="terminal-input"
-              contentEditable="true"
-              suppressContentEditableWarning={true}
-              onKeyDown={(e) => handleKeys(e)}
-              ref={answerRef}
-            ></span>
-            <button className="submitAnswer" onClick={handleButton}>
-              <i className="fa-solid fa-right-to-bracket"></i>
-            </button>
-          </div>
-          <div className="skipButton">
-            <span>Type your answer and press ENTER or </span>
-            <button onClick={skipQ}>skip</button>
-          </div>
+      {state.modalOutput && (
+        <div className="game-modal">
+          <div className="modal-content">{state.modalOutput}</div>
         </div>
-      </div>
+      )}
     </div>
   );
-}
+};
+
+export default Trivia;
